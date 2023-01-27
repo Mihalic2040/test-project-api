74f84c49c79732679940d6f0374f594005e68a02
Mihalic2040 <mihabevza111@gmail.com>
2023-01-27 22:32:49 +0200

    update

--- WEBAPP/src/API/api/tasks.py
+++ WEBAPP/src/API/api/tasks.py
@@ -1,11 +1,11 @@
 from celery import shared_task
-import os
-from celery import Celery 
+#import os
+#from celery import Celery 
 
 
-celery = Celery('tasks', broker='redis://redis:6379')
+#celery = Celery('tasks', broker='redis://redis:6379')
 
-os.environ["DJANGO_SETTINGS_MODULE"] = "main.settings"
+#os.environ["DJANGO_SETTINGS_MODULE"] = "main.settings"
 
 @shared_task()
 def log_console():
